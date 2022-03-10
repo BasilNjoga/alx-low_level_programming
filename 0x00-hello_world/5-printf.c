611b0d56377a5efb039f0a5475e5ccb67a3d4b5f
BasilNjoga <basilnjoga@gmail.com>
2022-03-10 14:33:25 -0800

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/*
+ * main - Entry point
+ *
+ *This program prints a text 
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+ * This program gives the size of values
+ *
+ * Return always 0 (successful)
+ */
+
+int main(void)
+{
+	printf("hellow word");
+
+	return (0);
+
+}
+
