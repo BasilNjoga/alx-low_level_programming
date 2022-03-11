09e4dd9ada806efe8bd0997df3c7aa1a0c8fcf27
BasilNjoga <basilnjoga@gmail.com>
2022-03-11 04:41:47 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * This function prints all the letters of the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 48; ch <= 57 ;ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	ch = '\n';
+	putchar(ch);
+	return (0);
+}
