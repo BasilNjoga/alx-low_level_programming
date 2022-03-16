0017654db9bf8f631d3543e32261b89a76ecb63d
BasilNjoga <63318694+BasilNjoga@users.noreply.github.com>
2022-03-17 01:30:01 +0300

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,23 @@
-#include <stdio.h>
-#include <unistd.h>
 #include "main.h"
+
 
 /**
  * main - Entry point
  *
- * This program outputs a word from using an external function
- *
- * Return: Always 0 (Success)
+ * Return: nothing, return void
  */
+
 int main(void)
 {
-	char nl;
-	int ch[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	for (int i = 0; i <= 7 ; i++)
-	{
-	putchar(ch[i]);
-	}
-	nl = '\n';
-	putchar(nl);
-
-	return (0);
+return (0);
 }
