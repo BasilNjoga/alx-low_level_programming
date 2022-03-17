a001b0bd17624ac0d3cf9ff864f0c2724656fb99
Basil Njoga <basilnjoga@gmail.com>
2022-03-17 14:18:41 -0700

    numbers times 10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints all numbers
+ *
+ * Return: Always (0) - success
+ */
+
+void more_numbers(void)
+{
+	char num;
+
+	for (int i = 0; i <= 10; i++)
+	{
+	for (num = 48; num <= 57; num++)
+	{
+	_putchar(num);
+	}
+	for (num = 48; num <= 52; num++)
+	{
+	_putchar(num + 10);
+	}
+	}
+	_putchar('\n');
+}
