653dab7ddcba6bca1b59d72217f2974c91e1f683
Basil Njoga <basilnjoga@gmail.com>
2022-03-18 14:49:09 -0700

    new line

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,6 +12,8 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
+	if (size > 0)
+	{
 	for (i = 1; i <= size; i++)
 	{
 		for (j = size; j > i; j--)
@@ -24,4 +26,8 @@ void print_triangle(int size)
 		}
 		_putchar('\n');
 	}
+	}
+	else
+	{
+	_putchar('\n');
 }
