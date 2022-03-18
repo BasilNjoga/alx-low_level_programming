e3b3c161fb53b6f73cf6f797ca94f7d4f7a6aec8
Basil Njoga <basilnjoga@gmail.com>
2022-03-18 03:23:46 -0700

    print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints squares
+ *
+ * @size: size of square required
+ *
+ * Return: Always (0) Success
+ */
+
+void print_square(int size)
+{
+	int j;
+	int i;
+
+	for (i = 1; i <= size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/test.c
+++ 0x04-more_functions_nested_loops/test.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-#include "3-print_numbers.c"
-#include "main.h"
-#include "1-isdigit.c"
-#include "5-more_numbers.c"
-#include "6-print_line.c"
+#include "7-print_diagonal.c"
+
 
 int main(void)
 {
-	print_line(6);	
-	return (0);
+	print_diagonal(7);
 }
