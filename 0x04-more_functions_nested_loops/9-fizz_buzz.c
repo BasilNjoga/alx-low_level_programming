240d14c1fc87914aa00a9c46586f3a05daa0bceb
Basil Njoga <basilnjoga@gmail.com>
2022-03-18 15:13:43 -0700

    fizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/*
+ * main - entry point
+ *
+ * This program prints numbers from 1 to 100 substituting
+ * multiples of 5 and 3 with words
+ *
+ * Return: Always (0) - success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+		printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+		printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+		printf("Buzz ");
+		}
+		else
+		{
+		printf("%d ", i);
+		}
+	}
+
+	return (0);
+}
