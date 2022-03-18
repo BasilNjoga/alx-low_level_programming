43758bb2dba6f8506da345c98c43a26871ba0e87
Basil Njoga <basilnjoga@gmail.com>
2022-03-18 15:47:39 -0700

    letters

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	printf("infinite loop incomiming :(\n");
+	printf("Infinite loop incomiming :(\n");
 
 	i = 0;
 
@@ -19,7 +19,7 @@ int main(void)
 	 *	putchar(i);
 	 * }
 	 */
-	printf("infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
