426ad987b65fe08ce273b3342830c7266e555758
Basil Njoga <basilnjoga@gmail.com>
2022-03-18 14:02:18 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -17,13 +17,12 @@ void print_square(int size)
 	{
 	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j < size; j++)
-		{
-		_putchar('#');
-		}
-		_putchar('\n');
+	for (j = 0; j < size; j++)
+	{
+	_putchar('#');
 	}
 	_putchar('\n');
+	}
 	}
 	else
 	{
