02aaf6b8f5e5c5f4df8a246b5346c89bf5187caa
Basil Njoga <basilnjoga@gmail.com>
2022-03-21 16:54:37 -0700

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * -strcpy - copies one string to another
+ *
+ * @dest: buffer string
+ *
+ * @src: source string
+ *
+ * Return: Always 0 - Success
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+	int i;
+
+	for (i = 0; i <= len; i++)
+	{
+	dest[i] = src[i] ;
+	}
+
+	return (dest);
+}
