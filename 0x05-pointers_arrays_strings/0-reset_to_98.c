fefebdffacfb724b37b49e8ab11f5f677fb6fdd3
Basil Njoga <basilnjoga@gmail.com>
2022-03-21 13:55:23 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - pupdates the value of a pointer
+ *
+ * @n: the parameter to be updated
+ *
+ * Return: Void - nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is an exercise on pointers strings and arrays
