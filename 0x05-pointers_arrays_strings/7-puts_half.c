af20d58e783e206eb281875922cb091cc6d30f5d
Basil Njoga <basilnjoga@gmail.com>
2022-03-21 16:17:47 -0700

    only half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * puts_half - prints half of a string
+ *
+ * strlen - gives the length of string
+ *
+ * @str: the string to be printed
+ *
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int len;
+	int i;
+	int halflen;
+
+	len = strlen(str);
+	if (len % 2 != 0)
+	{
+	halflen = (len - 1) / 2;
+	}
+	else
+	{
+	halflen = (len / 2);
+	}
+	for (i = halflen; i < len; i++)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
