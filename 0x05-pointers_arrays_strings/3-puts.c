4fc2d9715886fed9497433866b1a26a48f9101ab
Basil Njoga <basilnjoga@gmail.com>
2022-03-21 14:49:21 -0700

    print

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - prints a strint to the standar output
+ *
+ * strlen - gives the length of the string
+ *
+ * @str - string to be printed
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int len;
+	int i;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+}
