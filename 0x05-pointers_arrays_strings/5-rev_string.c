bf83a105464876933412377793498e7835ee1e9d
Basil Njoga <basilnjoga@gmail.com>
2022-03-21 15:25:50 -0700

    reverse shortcut

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses a string
+ *
+ * strrev - reverses a string
+ *
+ * @s: string to be reversed
+ *
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	strrev(s);
+}
