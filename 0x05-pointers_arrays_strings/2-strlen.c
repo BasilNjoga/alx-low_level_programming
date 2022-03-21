8578fedf94c092dac1404a985dd9c87edd7ce5dd
Basil Njoga <basilnjoga@gmail.com>
2022-03-21 14:34:18 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * strlen - returns the length
+ *
+ * @s: string to be measured
+ *
+ * Return: Always 0 - success
+ */
+
+int _strlen(char *s)
+{
+	int stringlen;
+
+	stringlen = strlen(s);
+	return (stringlen);
+}
