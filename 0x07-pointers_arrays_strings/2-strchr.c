564f7a55738b224b7fc1059e923ea1b7b951832f
Basil Njoga <basilnjoga@gmail.com>
2022-03-29 13:28:01 -0700

    check

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - locates a string
+ *
+ * @s:pointer to the first occurrence of the string
+ * @c: string to be searched for
+ *
+ * Return: Always 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+	int len;
+
+	len = strlen(s);
+	for (i = 0; i < len; i++)
+	{
+	if (s[i] == c)
+	{
+	return (s[i]);
+	}
+	else
+	{
+	return (NULL);
+	}
+	return (0);
+	}
+}
