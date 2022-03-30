2aba85df330d0cbee4cc7a99609a8db10fde193a
Basil Njoga <basilnjoga@gmail.com>
2022-03-30 15:01:57 -0700

    check

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _print_rev_recursion - prints a string followed by anew line
+ *
+ * @s: string to be printed
+ *
+ * Return: Nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int len;
+
+	len = strlen(s) - 1;
+	if (len == 0)
+	{
+	_putchar('\n');
+	return;
+	}
+	_putchar(s[len]);
+	len--;
+	_print_rev_recursion(s);
+}
