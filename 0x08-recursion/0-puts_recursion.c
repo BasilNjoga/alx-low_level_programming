1950b00e7ec368cf1230f2b94e258fa9532d518b
Basil Njoga <basilnjoga@gmail.com>
2022-03-30 14:31:22 -0700

    check

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by anew line
+ *
+ * @s: string to be printed
+ *
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+	return ;
+	}
+	_putchar(s[0]);
+	s = &s[1];
+	_puts_recursion(char *s);
+}
+
+	
