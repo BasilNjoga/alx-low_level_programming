57ac388fd6d58b2de14ecea892119b1a281d2966
Basil Njoga <basilnjoga@gmail.com>
2022-03-30 17:25:57 -0700

    inceptions

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns natural square root
+ *
+ * @n: intger to get square root of
+ *
+ * Return: always 0 - success
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks if a number is a prime number
+ *
+ * @n: number to be checked
+ *
+ * Return: always 0- success
+ */
+
+int is_prime_number(int n)
+{
+	if ( n % 2 != 0 && n % 3 != 0 && n % 5 != 0 && n % 7 != 0)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
+
