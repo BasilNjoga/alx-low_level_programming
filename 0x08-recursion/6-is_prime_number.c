fe953abdf19c76aaa716c531705dac2dab660aab
Basil Njoga <basilnjoga@gmail.com>
2022-03-30 17:28:08 -0700

    check

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,7 +10,11 @@
 
 int is_prime_number(int n)
 {
-	if ( n % 2 != 0 && n % 3 != 0 && n % 5 != 0 && n % 7 != 0)
+	if (n == 1)
+	{
+	return (0);
+	}
+	if (n % 2 != 0 && n % 3 != 0 && n % 5 != 0 && n % 7 != 0)
 	{
 	return (1);
 	}
