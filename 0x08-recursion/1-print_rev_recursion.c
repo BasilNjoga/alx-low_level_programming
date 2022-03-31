0df346f7082ba6a5f494837936fabbd6e9b3ab6c
BasilNjoga <63318694+BasilNjoga@users.noreply.github.com>
2022-04-01 00:14:32 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,11 +11,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (s[1] == '\0')
+	if (*s != '\0')
 	{
-	_putchar(s[0]);
-	return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 	}
-	s = &s[1];
-	_print_rev_recursion(s);
 }
