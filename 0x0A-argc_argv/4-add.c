c85a8486241d6acf2ce2cd0c2439c8e1741e5779
Basil <Basilnjoga@gmail.com>
2022-04-01 14:14:41 -0700

    isdigit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "main.h"
 
 /**
@@ -27,6 +28,11 @@ int main(int argc, char *argv[])
 	sum = 0;
 	for (i = 0; i < argc; i++)
 	{
+	if (!isdigit(argv[i]))
+	{
+	printf("Error\n");
+	return (1);
+	}
 	sum = sum + atoi(argv[i]);
 	}
 	printf("%d\n", sum);
