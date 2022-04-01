c473d2bc40e769bd74d46b9aa99fa09153d16ef7
Basil <Basilnjoga@gmail.com>
2022-04-01 07:46:37 -0700

    number

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include "main.h"
+#define UNUSED(x) (void)(x)
 
 /**
  * main - entry point
  *
  * This program prints out the number of arguments
+ *@argc: character count
+ *@argv: array of characters
  *
  * Return: always 0
  */
 
-int main (int argc, int argv[])
+int main(int argc, int argv[])
 {
-	printf("%d\n",argc);
+	UNUSED(argv);
+	printf("%d\n", argc);
 	return (0);
 }
