600d316dd28e4b5fee1d4a5ee669408156b7e2ed
Basil <Basilnjoga@gmail.com>
2022-04-01 08:44:46 -0700

    check

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: count of number of arguments
+ * @argv: array of strings
+ *
+ * This program multiplies two integers
+ *
+ * Return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int mult;
+
+	if (argc < 3)
+	{
+	printf("Error\n");
+	return 1;
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mult = num1 * num2;
+	printf("%d\n", mult);
+	return 0;
+}
