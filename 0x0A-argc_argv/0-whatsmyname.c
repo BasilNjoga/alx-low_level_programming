5acdcd2c0e9de4be66bb2ca79d0e28f0e7eac8a7
Basil <Basilnjoga@gmail.com>
2022-04-01 06:56:05 -0700

    check

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,12 +7,15 @@
  *
  * This program simply prints its name
  *
+ *@argc: number count of command line arguments
+ *@argv: array of characters
+ *
  * Return: always 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	UNUSED(argc);
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
