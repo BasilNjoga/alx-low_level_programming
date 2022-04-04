f14479352488e5953b1a5d8fc9294ef8da51e35e
Basil <Basilnjoga@gmail.com>
2022-04-04 14:49:02 -0700

    improved malloc return value

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -23,11 +23,14 @@ char *create_array(unsigned int size, char c)
 	else
 	{
 	str = malloc(sizeof(char) * size);
+	if (str == NULL)
+	{
+	return (0);
+	}
 	for (i = 0; i < size; i++)
 	{
 		str[i] = c;
 	}
 	return (str);
 	}
-	return (0);
 }
