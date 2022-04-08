e868cc87a74c58a75e793b56372d23bda2594b90
BasilNjoga <63318694+BasilNjoga@users.noreply.github.com>
2022-04-09 00:09:36 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,29 +1,47 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * alloc_grid - creates a 2 dimensinal array of integers
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: columns
+ * @height: rows
  *
- * @width: first dimension
- *
- * @height: second dimension
- *
- * Return: always 0
+ * Return: Double Pointer
  */
 
-int **alloc_grid(int width,int height)
+int **alloc_grid(int width, int height)
 {
-	int **grid;
-	int i, j;
+	int **s;
+	int i, j, k, fr;
 
-	grid = malloc(sizeof(int) * width * height);
-	for (i = 0;i < width; i++)
+	fr = 0;
+	if (width <= 0 || height <= 0)
+		return (0);
+
+	s = malloc(height * sizeof(int *));
+	if (s == 0)
+		return (0);
+	for (i = 0; i < height; i++)
 	{
-		for ( j = 0; j < height; j++)
+		*(s + i) = malloc(width * sizeof(int));
+		if (*(s + i) == 0)
 		{
-		grid[i][j] = 0;
+			fr = 1;
+			break;
+		}
+		for (j = 0; j < width; j++)
+		{
+			s[i][j] = 0;
 		}
 	}
-	return (grid);
+	if (fr == 1)
+	{
+		for (k = 0; k <= i; k++)
+		{
+			free(*(s + k));
+		}
+		free(s);
+	}
+	return (s);
 }
