243fe33b2d524bbfbf8e8a799289b398981b5269
BasilNjoga <63318694+BasilNjoga@users.noreply.github.com>
2022-04-09 00:37:25 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,47 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * _memset -  initializes n bytes of memory to x
- * @ptr: initial adress
- * @x: variable to initialize with
- * @n: number of bytes to initialize
- *
- * Return: Return pointer char (so movements are 1 byte)
- */
-char *_memset(char *ptr, int x, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		ptr[i] = x;
-
-	return (ptr);
-}
 
 /**
- * _calloc -  allocates memory using malloc and initializes in 0
- * @nmemb: number of elements of array to allocate
- * @size: size of elements
+ * string_nconcat -  concatenates two strings
+ * @s1: string1
+ * @s2: string2
+ * @n: n bytes of string 2
  *
- * Return: Pointer to allocated memory or normal process termination
- * with a status value of 98
+ * Return: Pointer to allocated memory of s1 + nbytes of s2
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	void *p;
+	unsigned int l1, l2, i, j;
+	char *s;
+	char *nul = "";
 
+	if (s1 == NULL)
+		s1 = nul;
+	if (s2 == NULL)
+		s2 = nul;
 
-	if (nmemb == 0 || size == 0)
+	l1 = 0, l2 = 0;
+	while (*(s1 + l1))
+		l1++;
+	while (*(s2 + l2))
+		l2++;
+
+	if (n < l2)
+		l2 = n;
+
+	s = malloc(sizeof(char) * (l1 + l2 + 1));
+
+	if (s == 0)
 		return (0);
 
-	p = malloc(nmemb * size);
-	if (p == 0)
-		return (0);
-	_memset(p, 0, size * nmemb);
+	for (i = 0; i < l1; i++)
+		*(s + i) = *(s1 + i);
 
-	return (p);
+	for (i = 0, j = l1; i < l2; j++, i++)
+		*(s + j) = *(s2 + i);
+
+	*(s + j) = '\0';
+
+	return (s);
 }
