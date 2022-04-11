a157f1036d237977b7bb8edb01898d1432e6ed7f
Basil <Basilnjoga@gmail.com>
2022-04-11 15:38:10 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/**
+ * struct dog - gives dog details
+ *
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: this gives the name of the dog, age and owners name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
