2c96b754de8b55d2239d7685f17782b44f51ac00
Basil <Basilnjoga@gmail.com>
2022-04-11 15:01:26 -0700

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * this program returns the name of the file
+ *
+ * Return: Always 0 - success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
