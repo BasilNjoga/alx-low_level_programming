71dcd44753961a206d6c80d02473687953a91bfc
Basil Njoga <Basilnjoga@gmail.com>
2022-04-23 00:27:01 +0300

    Singly linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints a list of things in a string
+ *
+ * @h: list to be printed
+ *
+ * Return: Always (0)
+ */
+
+size_t print_list(const list_t *h)
+{
+	return (0);
+	
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is a problem exercise on singly linked lists
