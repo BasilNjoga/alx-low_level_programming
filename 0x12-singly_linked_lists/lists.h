ace8698ca7564bdd28d5c2040345a382c749051d
Basil Njoga <Basilnjoga@gmail.com>
2022-04-25 23:42:46 +0300

    check

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
 
+typedef struct list_t
+{
+	size_t len;
+	char *str;
+	struct list_t *next;
+}list_t;
 size_t print_list(const list_t *h);
-
 #endif
