7a3cc356451c118237ae1f8510bf17c80f835301
Basil Njoga <Basilnjoga@gmail.com>
2022-05-04 01:15:55 +0300

    check

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * ssize_t = this is a function to read a file
+ * 
+ * @filename: this is the first paramert 
+ * @letters: this is the second parameter 
+ * @Return: ssize_t 
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd;
+    char buf[letters + 1];
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        return (0);
+    }
+    read(fd, buf, letters);
+    buf[letters] = '\0';
+
+    return (0);
+
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,1 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
