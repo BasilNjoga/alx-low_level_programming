82a2537a8a980acc5aa7c1ca6f23e0e66fbc6b8e
Basil Njoga <Basilnjoga@gmail.com>
2022-05-04 02:18:30 +0300

    stuff

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
     int fd;
     int i;
 
-    fd = open (filename, O_CREAT | O_WRONLY , 0600);
+    fd = open(filename, O_CREAT | O_WRONLY , 0600);
     while (text_content[i] != '\0')
     {
         write(fd, text_content[i], 1);
