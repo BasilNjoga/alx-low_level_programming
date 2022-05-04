4c65eaa9bb09ffb506eede9a1089a56a7c05fedf
BasilNjoga <63318694+BasilNjoga@users.noreply.github.com>
2022-05-05 01:23:08 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-#include <unistd.h>
 #include "main.h"
-
 /**
- * ssize_t = this is a function to read a file
- * 
- * @filename: this is the first paramert 
- * @text_content: this is the second parameter 
- * @Return: create_File 
- */
+  * _strlen - length of a string
+  * @s: input char
+  * Return: length of a string
+**/
+int _strlen(char *s)
+{
+	int i = 0;
 
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+* create_file - check the code for Holberton School students.
+* @filename: file to create.
+* @text_content: info to write into the file.
+* Return: 1 on success, -1 on failure
+*/
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    int i;
+	ssize_t nletters;
+	int file;
 
-    fd = open(filename, O_CREAT | O_WRONLY , 0600);
-    while (text_content[i] != '\0')
-    {
-        write(fd, text_content[i], 1);
-        i++;
-    }
-
-    return fd;
+	if (!filename)
+		return (1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		nletters = write(file, text_content, _strlen(text_content));
+		if (nletters == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	close(file);
+	return (1);
 }
