943d4962934b67655a7db56914cf909df5342c08
BasilNjoga <63318694+BasilNjoga@users.noreply.github.com>
2022-05-05 01:23:47 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,30 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-#include <unistd.h>
 #include "main.h"
 
 /**
- * ssize_t = this is a function to read a file
- * 
- * @filename: this is the first paramert 
- * @text_content: this is the second parameter 
- * @Return: create_File 
- */
+  * _strlen - length of a string
+  * @s: input char
+  * Return: length of a string
+**/
+int _strlen(char *s)
+{
+	int i = 0;
 
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+* append_text_to_file - appends text at the end of a file.
+* @filename: file to append.
+* @text_content: info to append into the file.
+* Return: 1 on success, -1 on failure
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
+	ssize_t nletters;
+	int file;
 
-    fd = open(filename, O_WRONLY , 0600);
-
-    if (fd == -1)
-{
-    return -1;
-}
-
+	if (!filename)
+		return (-1);
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		nletters = write(file, text_content, _strlen(text_content));
+		if (nletters == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	close(file);
+	return (1);
 }
