ff0db258ffcb28ddf460fde5bf6e5e6a0a104b84
BasilNjoga <63318694+BasilNjoga@users.noreply.github.com>
2022-05-05 01:26:12 +0300

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,22 +1,25 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void validate_elf(unsigned char *e_ident);
+unsigned int lit_to_big_endian(unsigned int x);
+void print_entry(unsigned int e_type, unsigned char *e_ident);
 
-#endif
+#endif /* MAIN_H */
