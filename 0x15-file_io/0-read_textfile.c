7e686cb5ac45dd57272c10e240dea73db5f9f79e
BasilNjoga <63318694+BasilNjoga@users.noreply.github.com>
2022-05-05 01:22:25 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-#include <unistd.h>
 #include "main.h"
 
 /**
- * ssize_t = this is a function to read a file
- * 
- * @filename: this is the first paramert 
- * @letters: this is the second parameter 
- * @Return: ssize_t 
- */
-
+* read_textfile - check the code for Holberton School students.
+* @filename: file to read and write
+* @letters: number of letters to read and write.
+* Return: letters printed
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    char buf[letters + 1];
+	ssize_t nletters;
+	int file;
+	char *text;
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        return (0);
-    }
-    read(fd, buf, letters);
-    buf[letters] = '\0';
-
-    return (0);
-
+	if (!filename)
+		return (0);
+	text = malloc(sizeof(char) * letters + 1);
+	if (text == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
+	nletters = read(file, text, sizeof(char) * letters);
+	if (nletters == -1)
+	{
+		free(text);
+		close(file);
+		return (0);
+	}
+	nletters = write(STDOUT_FILENO, text, nletters);
+	if (nletters == -1)
+	{
+		free(text);
+		close(file);
+		return (0);
+	}
+	free(text);
+	close(file);
+	return (nletters);
 }
