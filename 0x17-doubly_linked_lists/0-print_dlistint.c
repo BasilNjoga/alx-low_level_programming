546d7d00681c827eadd01e68ade8f2f3f7647391
Basil Njoga <Basilnjoga@gmail.com>
2022-06-11 00:10:50 +0300

    check

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_dlisting - this function prints all elements of a singly linked list
+ *
+ * @h: pointer to a list
+ *
+ * Return: nothing
+ *
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	dlistint_t counter;
+	int i;
+
+
+}
