c0f6b1de86f7256d1ff181a11557a3f3dbceb6e2
Basil Njoga <Basilnjoga@gmail.com>
2022-06-11 01:31:43 +0300

    check

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,16 +8,15 @@
  *
  * @h: pointer to a list
  *
- * Return: nothing
- *
+ * Return: nothing *
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *counter;
 	size_t i = 0;
-
 	counter = h;
+	
 	while(counter != NULL)
 	{
 		i++;
