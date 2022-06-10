1a568f90568a528a1c405f17e32f69c647733d96
Basil Njoga <Basilnjoga@gmail.com>
2022-06-11 00:31:11 +0300

    check

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,12 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+typedef struct dlistint_t
+{
+    dlistint_t *next;
+    dlistint_t *prev;
+} dlistint_t;
+
 size_t print_dlistint(const dlistint_t *h);
 
 #endif
