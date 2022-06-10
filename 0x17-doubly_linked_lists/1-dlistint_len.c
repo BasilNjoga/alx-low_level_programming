7f724e97ae782783f20ff922b159380d440e9d23
Basil Njoga <Basilnjoga@gmail.com>
2022-06-10 23:40:22 +0300

    check

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * add_dnodeint - adds a new node at the beginning
+ * 
+ * @head: pointer to the beginning
+ * 
+ * @n: number
+ * 
+ * Return: Always 0 - success
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+    
+}
