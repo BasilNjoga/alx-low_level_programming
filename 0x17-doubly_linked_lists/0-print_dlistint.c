0beccc4021d2a3ceddede789ada3944907450f01
Basil Njoga <Basilnjoga@gmail.com>
2022-06-16 11:20:03 +0300

    check

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -21,30 +21,7 @@ size_t print_dlistint(const dlistint_t *h)
 	{
 		i++;
 		counter = counter->next;
+        printf("%d",counter->n)
 	}
 	return(i);
-}
-int main(void)
-{
-    dlistint_t *head;
-    dlistint_t *new;
-    dlistint_t hello = {8, NULL, NULL};
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    {
-        dprintf(2, "Error: Can't malloc\n");
-        return (EXIT_FAILURE);
-    }
-    new->n = 9;
-    head->prev = new;
-    new->next = head;
-    new->prev = NULL;
-    head = new;
-    n = print_dlistint(head);
-    printf("-> %lu elements\n", n);
-    free(new);
-    return (EXIT_SUCCESS);
 }
