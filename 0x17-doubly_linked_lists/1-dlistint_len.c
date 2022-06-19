2b399030cd069505276e7e41c11cec4b7e0a2301
Basil Njoga <Basilnjoga@gmail.com>
2022-06-19 13:56:10 +0300

    check

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,7 +4,7 @@
 #include "lists.h"
 
 /**
- * print_dlisting - this function prints all elements of a singly linked list
+ * dlistint_len - this function prints all elements of a singly linked list
  *
  * @h: pointer to a list
  *
@@ -13,15 +13,15 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-    const dlistint_t *counter;
+	const dlistint_t *counter;
 	size_t i = 0;
 	counter = h;
 
-	while(counter != NULL)
+	while (counter != NULL)
 	{
 		i++;
 		counter = counter->next;
 	}
-	return(i);
+	return (i);
 }
 
