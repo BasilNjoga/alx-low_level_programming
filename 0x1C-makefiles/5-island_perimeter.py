63e024213f56dc9cf6576b6739f18c390af8f997
Basil Njoga <Basilnjoga@gmail.com>
2022-08-18 02:24:47 +0300

    check

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" This is a module that computes the perimeter of an island given a grid which is a list of integers"""
+""" This is a module that computes the perimeter of an
+island given a grid which is a list of integers"""
 
 
 def island_perimeter(grid):
@@ -8,4 +9,4 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             x += grid[i][j]
-    return x * 2
+    return (x + 1) * 2
