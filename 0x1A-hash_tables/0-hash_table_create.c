4002588b958d12f083c3a87c96bbdfd771a0abd6
Basil Njoga <Basilnjoga@gmail.com>
2022-08-17 18:41:03 +0300

    check

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * @brief 
+ * 
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t my_table[10];
+    
+
+}
