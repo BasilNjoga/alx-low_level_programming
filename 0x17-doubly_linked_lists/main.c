a8c69daffc8a6dbdb336637719589d7820cc209b
Basil Njoga <Basilnjoga@gmail.com>
2022-09-13 20:45:39 +0300

    check

--- 0x17-doubly_linked_lists/main.c
+++ 0x17-doubly_linked_lists/main.c
@@ -1,26 +1,54 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
+size_t _print_dlistint_backward(const dlistint_t *h);
+
 /**
- * main - check the code
+ * _free_dlist - Realease the memory allocated for a list
  *
- * Return: Always EXIT_SUCCESS.
+ * @head: A pointer to the first node of the list to free
+ */
+void _free_dlist(dlistint_t *head)
+{
+	if (head)
+	{
+		_free_dlist(head->next);
+		free(head);
+	}
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-    dlistint_t *head;
+	dlistint_t *head;
+	int ints[2] = {
+		0,
+		9
+	};
+	dlistint_t *ptr;
+	int i;
+	size_t n;
 
-    head = NULL;
-    add_dnodeint(&head, 0);
-    add_dnodeint(&head, 1);
-    add_dnodeint(&head, 2);
-    add_dnodeint(&head, 3);
-    add_dnodeint(&head, 4);
-    add_dnodeint(&head, 98);
-    add_dnodeint(&head, 402);
-    add_dnodeint(&head, 1024);
-    print_dlistint(head);
-    return (EXIT_SUCCESS);
+	head = NULL;
+	for (i = 0; i < 2; ++i)
+	{
+		ptr = add_dnodeint(&head, ints[i]);
+		if (!ptr)
+		{
+			printf("Failed\n");
+			_free_dlist(head);
+			return (1);
+		}
+	}
+	n = print_dlistint(head);
+	printf("-> %lu elements\n", n);
+	n = _print_dlistint_backward(head);
+	printf("-> %lu elements\n", n);
+	_free_dlist(head);
+	return (0);
 }
