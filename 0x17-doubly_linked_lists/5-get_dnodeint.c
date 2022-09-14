be1d8c3634be1f8942d86a485672fd6be9a0ca56
Basil Njoga <Basilnjoga@gmail.com>
2022-09-14 22:04:33 +0300

    chekc

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
     while (i != index)
     {
         head = head->next;
-        i++
+        i++;
     }
     return head;
 }
