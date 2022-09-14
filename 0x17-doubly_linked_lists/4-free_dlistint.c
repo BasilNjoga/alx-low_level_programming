0d13e31c8b258b17282b0ca12a03339c4db78778
Basil Njoga <Basilnjoga@gmail.com>
2022-09-14 22:00:07 +0300

    check

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * free_dlistint - frees up a linked list
+ * 
+ * @head: The head to the linked list to free
+ * 
+ * Return: nothing
+ */
 void free_dlistint(dlistint_t *head)
 {
     dlistint_t *temp;
---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    
+}
