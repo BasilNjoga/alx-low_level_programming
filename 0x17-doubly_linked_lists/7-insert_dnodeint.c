d50aa72fd33d537d40df40cc5992afda26fd9cc2
Basil Njoga <Basilnjoga@gmail.com>
2022-09-14 22:14:53 +0300

    check

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a created node at an index
+ * 
+ * @h: pointer to the head of the list
+ * @idx: index of the list to be added
+ * @n: integer value of node
+ *  
+ * @Return: dlistint_t* 
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+
+}
