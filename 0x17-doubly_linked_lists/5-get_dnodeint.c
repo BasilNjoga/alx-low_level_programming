1ff0d3083762d1e18becaffba7103febf0c1ffde
Basil Njoga <Basilnjoga@gmail.com>
2022-09-14 22:04:07 +0300

    chekc

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -2,7 +2,25 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index - Get the dnodeint at index object
+ *
+ * @head: list to be checked
+ * @index: index of node to be gotter
+ * 
+ * @Return: dlistint_t* 
+ */
+
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    
+    dlistint_t *temp;
+    unsigned int i;
+
+    i = 0;
+    while (i != index)
+    {
+        head = head->next;
+        i++
+    }
+    return head;
 }
