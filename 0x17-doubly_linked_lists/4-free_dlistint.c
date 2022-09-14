560aab8da3efa5e35ad0b2c67f0bfadb3edfbcda
Basil <Basilnjoga@gmail.com>
2022-09-14 15:12:38 -0700

    check

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -4,20 +4,20 @@
 
 /**
  * free_dlistint - frees up a linked list
- * 
+ *
  * @head: The head to the linked list to free
- * 
+ *
  * Return: nothing
  */
+
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *temp;
-    
-    while(head != NULL)
-    {
-        temp = head;
-        head = head->next;
-        free(temp);
-    }
+	dlistint_t *temp;
 
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
 }
