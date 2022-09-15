ac92681665539e54befefdd491db840b6e1663ff
Basil <Basilnjoga@gmail.com>
2022-09-15 06:18:38 -0700

    check

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,9 +32,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		i++;
 	}
 	new_node->next = temp->next;
+	temp->next->prev = new_node;
 	new_node->prev = temp;
 	temp->next = new_node;
-	new_node->next->prev = new_node;
 
 	return (new_node);
 }
