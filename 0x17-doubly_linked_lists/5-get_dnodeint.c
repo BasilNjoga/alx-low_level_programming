35cefa012b89d2e744048be9c01b371f0974ecff
Basil <Basilnjoga@gmail.com>
2022-09-15 03:22:37 -0700

    chekc

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -32,5 +32,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		head = head->next;
 		i++;
 	}
-	return (head);
+	return (NULL);
 }
--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -4,26 +4,25 @@
 
 /**
  * sum_dlistint - returns the sum of n values
- * 
+ *
  * @head: The head of the list to be summed
- * 
- * @Return: int 
+ *
+ * Return: int
  */
 
 int sum_dlistint(dlistint_t *head)
 {
-    int sum;
+	int sum;
 
-    if (head == NULL)
-    {
-        return (0);
-    }
-    sum = 0;
-    while (head != NULL)
-    {
-        sum = sum + head->n;
-        head = head->next;
-    }
-
-    return (sum);
+	if (head == NULL)
+	{
+		return (0);
+	}
+	sum = 0;
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
 }
