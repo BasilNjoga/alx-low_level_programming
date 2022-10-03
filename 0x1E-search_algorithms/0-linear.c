9cdd1525aacb0b15e358ea7aaf2ee287efcb40c6
basil <basilnjoga@gmail.com>
2022-10-03 06:03:12 -0700

    hceck

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,27 +4,28 @@
 
 /**
  * linear_search - this is a function to do a linear search
- * 
+ *
  * @array: first element of the array to search in
  * @size: number of elements in the array
  * @value: value to search for
- * 
- * Return - int 
+ *
+ * Return: int
  */
 
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
-    if (array == NULL)
-    {
-        return (-1);
-    }
-    for (i = 0; i < size; i++)
-    {
-        if (value == array[i])
-        {
-            return i;
-        }
-    }
-return (-1);
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (value == array[i])
+		{
+			return (i);
+		}
+	}
+	return (-1);
 }
