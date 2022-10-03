62c144e94678f8e8da5c062b7f6c034d8280f293
basil <basilnjoga@gmail.com>
2022-10-03 07:31:12 -0700

    hceck

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -3,41 +3,42 @@
 #include "search_algos.h"
 
 /**
- * binary_search - This function perfoms a binary search 
- * 
+ * binary_search - This function perfoms a binary search
+ *
  * @array: pointer to the first element of an array
  * @size: number of elements in the array
  * @value: value to be searched
- * 
- * Return: int 
+ *
+ * Return: int
  */
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t l;
-    size_t r;
-    size_t mid;
+	size_t l;
+	size_t r;
+	size_t mid;
 
-    if (array == NULL)
-    {
-        return (-1);
-    }
-    r = size - 1;
-    l = 0;
-    while (l <= r)
-    {   mid = floor((l + r)/2);
-        if (value == array[mid])
-        {
-            return (mid);
-        }
-        else if (value < array[mid])
-        {
-            l = mid - 1;
-        }
-        else 
-        {
-            r = mid + 1;
-        }
-    }
-    return (-1);
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	r = size - 1;
+	l = 0;
+	while (l <= r)
+	{
+		mid = floor((l + r) / 2);
+		if (value == array[mid])
+		{
+			return (mid);
+		}
+		else if (value < array[mid])
+		{
+			l = mid - 1;
+		}
+		else
+		{
+			r = mid + 1;
+		}
+	}
+	return (-1);
 }
