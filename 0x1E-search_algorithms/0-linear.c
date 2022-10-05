c83a1ee79672b279c8835cc3535b778e102235dd
Basil Njoga <Basilnjoga@gmail.com>
2022-10-05 16:11:36 +0300

    check

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -22,6 +22,7 @@ int linear_search(int *array, size_t size, int value)
 	}
 	for (i = 0; i < size; i++)
 	{
+		printf("value checked arrray[%d] = [%d]\n", i, array[i]);
 		if (value == array[i])
 		{
 			return (i);
